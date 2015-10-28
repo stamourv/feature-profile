ef1ee21c9974b8c9b109a0bed5bff4655116c094
Vincent St-Amour <stamourv@racket-lang.org>
2015-10-28 17:16:13 -0500

    Better printed representation for features.

--- structs.rkt
+++ structs.rkt
@@ -2,7 +2,11 @@
 
 (provide (all-defined-out))
 
-(struct feature (name key grouper analysis))
+(struct feature (name key grouper analysis)
+        #:methods gen:custom-write
+        [(define (write-proc f port mode)
+           (write-string (format "#<feature: ~s>" (feature-name f))
+                         port))])
 
 (struct feature-report
   (feature core-samples raw-samples grouped-samples total-time feature-time))
